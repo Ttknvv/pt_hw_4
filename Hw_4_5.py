26a6d98246782a19f788054c95309053b20f2fbb
Vova <vvvova24@gamil.com>
2022-10-18 18:12:18 +0600

    Hw_4_5_1

--- Hw_4_5.py
+++ Hw_4_5.py
@@ -1,1 +1,17 @@
 ##
+
+mnog = "mnog.txt"
+mnog2 = "mnog2.txt"
+first = ""
+second = ""
+data = open(mnog, "r")
+data2 = open(mnog2, "r")
+for i in data:
+    first = i
+for j in data2:
+    second = j
+data3 = open("mnog3.txt", "a")
+data3.writelines(first[:len(first)-2]+"+"+second)
+data.close()
+data2.close()
+data3.close()
